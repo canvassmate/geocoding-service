6b7eed2e58f9237c5c45764f0bed51fb6a72c322
Matias Pequeno <matias.pequeno@gmail.com>
2014-08-07 17:11:20 -0700

    Added some test routes.

--- app/routes/index.js
+++ app/routes/index.js
@@ -12,10 +12,22 @@
 
 var express = require('express');
 var router = express.Router();
+var log = config.getLogger('router');
 
 // GET home page.
 router.get('/', function(req, res) {
 	res.render('index', { title: 'Canvassmate Geocoding Web Service' });
+});
+
+// Simple check
+router.get('/api', function(req, res) {
+	res.send('API is running');
+});
+
+// Error example
+router.get('/error', function(req, res, next) {
+	log.error("Z0MG!1 Error!");
+	next(new Error('WTF Error!'));
 });
 
 module.exports = router;
