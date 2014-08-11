30e44835a5cd6e6c65bf5ded6dc72c8641bb7e10
Matias Pequeno <matias.pequeno@gmail.com>
2014-08-10 20:48:16 -0700

    Added more runtime facilities.

--- app/config/runtime.js
+++ app/config/runtime.js
@@ -11,10 +11,39 @@
  */
 'use strict';
 
+const log = config.logger('runtime');
+
+/**
+  Runtime utility
+
+  @class runtime
+ */
+
+module.exports.getClassName = getClassName;
+
+/**
+  Adds a function to the base Object prototype that returns the actual name
+  of the receiving class.
+
+  @example
+  `console.log(runtime.getClassName(obj));`
+
+  @method getClassName
+  @param {Object} obj Object to get the name from
+  @return {String} Name of the object formatted as [Object Name]
+ */
+function getClassName(obj) {
+   var results = /function (.{1,})\(/.exec(obj.constructor.toString());
+   var hasResults = results && results.length > 1;
+   return '[object ' + (hasResults ? results[1] : 'unknown') + ']';
+}
+
 /**
   Get the stack as an array of call sites.
+
+  @method getStack
  */
-module.exports.getStack = function getStack() {
+module.exports.getStack = function() {
   var limit = Error.stackTraceLimit;
   var obj = {};
   var prep = Error.prepareStackTrace;
@@ -32,4 +61,23 @@ module.exports.getStack = function getStack() {
   Error.stackTraceLimit = limit;
 
   return stack;
+};
+
+/**
+  Prints all keys (properties) and values of a given Object
+
+  @method getStack
+ */
+module.exports.logP = function(obj, tag, logF) {
+  logF = typeof logf === 'undefined' ? log.debug.bind(log) : logF;
+  tag = typeof tag !== 'undefined' ? ' ' + tag + ' ' : ' ';
+
+  var logmsg = getClassName(obj) + tag + '\x1B[37m{\n';
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      logmsg += '\t\x1B[37m' + key + ': \x1b[90m\'' + obj[key] + '\'\x1B[39m\n';
+    }
+  }
+
+  logF(logmsg + '}', 1);
 };
